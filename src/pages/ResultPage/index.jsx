7bfbcfcee7d33f9b82e55943bd6645b7cb09b5e3
Melrick1 <melrickbatuwael@gmail.com>
2025-04-24 16:25:14 +0800

    fix bug

--- src/pages/ResultPage/index.jsx
+++ src/pages/ResultPage/index.jsx
@@ -20,7 +20,7 @@ function Result () {
 
     return(
         <Layout pageName={"Hasil"}>
-            <section className="Result">
+            <main className="Result">
                 <div className="result-containers image">
                     <button className="button go-back" onClick={() => {navigate(-1)}}>
                         Kembali
@@ -33,7 +33,7 @@ function Result () {
                 <div className='result-containers pest-description'>
                     <ReactMarkdown>{analysisResult}</ReactMarkdown>
                 </div>
-            </section>
+            </main>
         </Layout>
     )
 };
