d4ecb5cde8a61af27a2c29c66d16b18719b906f8
Melrick1 <melrickbatuwael@gmail.com>
2025-04-24 16:44:41 +0800

    2nd bug fix

--- src/pages/AuthenticationPage/ForgotPass.jsx
+++ src/pages/AuthenticationPage/ForgotPass.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router'
 import { ResetPasswordReq } from '../../utilities/AuthManager';
-import InputText from '../../components/InputText';
 import InputEmail from '../../components/InputEmail';
 
 function ForgotPass () {
